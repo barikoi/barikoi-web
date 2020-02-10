4db8f82069eb40c639b76ea767fbb750cb0ab1b2
Asad Zaman <asadzaman666@outlook.com>
2020-02-10 14:53:54 +0600

    pricing UI update

--- src/app/components/pricing/pricing-calculator/pricing-calculator.component.ts
+++ src/app/components/pricing/pricing-calculator/pricing-calculator.component.ts
@@ -2,34 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { Options } from 'ng5-slider';
 
 @Component({
-  selector: 'app-pricing-calculator',
-  templateUrl: './pricing-calculator.component.html',
-  styleUrls: ['./pricing-calculator.component.scss']
+    selector: 'app-pricing-calculator',
+    templateUrl: './pricing-calculator.component.html',
+    styleUrls: ['./pricing-calculator.component.scss'],
 })
 export class PricingCalculatorComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    value = 12;
+    options: Options = {
+        floor: 0,
+        ceil: 12,
+        showSelectionBar: true,
+        getSelectionBarColor: (value: number): string => {
+            if (value <= 3) {
+                return 'red';
+            }
+            if (value <= 6) {
+                return 'orange';
+            }
+            if (value <= 9) {
+                return 'yellow';
+            }
+            return '#2AE02A';
+        },
+    };
 
-  value: number = 12;
-  options: Options = {
-    floor: 0,
-    ceil: 12,
-    showSelectionBar: true,
-    getSelectionBarColor: (value: number): string => {
-      if (value <= 3) {
-          return 'red';
-      }
-      if (value <= 6) {
-          return 'orange';
-      }
-      if (value <= 9) {
-          return 'yellow';
-      }
-      return '#2AE02A';
-    }
-  };
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
